817fc4ced183b37cfc9a26f1d740a17b9e046b45
gu_gu <38068491+brian4462@users.noreply.github.com>
2022-08-01 21:47:11 +0900

    Add files via upload

---
+++ precache-manifest.dc71b165c5d02285f237138479f78a30.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "845b54cee2795b8599145d8db9a6344b",
+    "url": "/wedding-card/index.html"
+  },
+  {
+    "revision": "9da203ec4bd048a46616",
+    "url": "/wedding-card/static/css/2.1056b32c.chunk.css"
+  },
+  {
+    "revision": "137dcc69da0b5164f74a",
+    "url": "/wedding-card/static/css/main.f1d579e4.chunk.css"
+  },
+  {
+    "revision": "9da203ec4bd048a46616",
+    "url": "/wedding-card/static/js/2.0f53858d.chunk.js"
+  },
+  {
+    "revision": "137dcc69da0b5164f74a",
+    "url": "/wedding-card/static/js/main.6cf6b2ef.chunk.js"
+  },
+  {
+    "revision": "073dcc65ade7ef8554aa",
+    "url": "/wedding-card/static/js/runtime~main.f1657230.js"
+  },
+  {
+    "revision": "993e286519f349cfc8d57487fcd3a485",
+    "url": "/wedding-card/static/media/1.993e2865.jpg"
+  },
+  {
+    "revision": "b91ed5a09efe49474dc7f2c607bd86b9",
+    "url": "/wedding-card/static/media/1.b91ed5a0.jpg"
+  },
+  {
+    "revision": "da2f5e097441a36c6983db1bc877a8b5",
+    "url": "/wedding-card/static/media/1.da2f5e09.jpg"
+  },
+  {
+    "revision": "ff94e000d29979f6e139ae3c72a8f334",
+    "url": "/wedding-card/static/media/1.ff94e000.jpg"
+  },
+  {
+    "revision": "3b18046c53ac126d1ee841114177144e",
+    "url": "/wedding-card/static/media/2.3b18046c.png"
+  },
+  {
+    "revision": "566092d12993858b15412cfe35f17b26",
+    "url": "/wedding-card/static/media/2.566092d1.jpg"
+  },
+  {
+    "revision": "e22a409e929ec29f39556b5e1a2d41e0",
+    "url": "/wedding-card/static/media/2.e22a409e.jpg"
+  },
+  {
+    "revision": "153fdd56540f101b3ea1b0385e4b2e3c",
+    "url": "/wedding-card/static/media/3.153fdd56.jpg"
+  },
+  {
+    "revision": "8cea517b8a32e30f392686ac0e10eb8c",
+    "url": "/wedding-card/static/media/4.8cea517b.jpg"
+  },
+  {
+    "revision": "b1fadc5daac506d84765097f2fca28a5",
+    "url": "/wedding-card/static/media/5.b1fadc5d.jpg"
+  },
+  {
+    "revision": "a0e6ecfd95433a58fed32090ba73ff34",
+    "url": "/wedding-card/static/media/6.a0e6ecfd.jpg"
+  },
+  {
+    "revision": "20428713710c3388010313bf1bf9bd56",
+    "url": "/wedding-card/static/media/7.20428713.jpg"
+  },
+  {
+    "revision": "ba807314686d1f39905c7ed526161445",
+    "url": "/wedding-card/static/media/8.ba807314.jpg"
+  },
+  {
+    "revision": "0129ad1762006b591bbb81312751496a",
+    "url": "/wedding-card/static/media/9.0129ad17.jpg"
+  },
+  {
+    "revision": "0917889dd5ca03c27b3b9412557a9fdc",
+    "url": "/wedding-card/static/media/Flaticon.0917889d.svg"
+  },
+  {
+    "revision": "15cf01b77e18000ff8c30e5eb8132326",
+    "url": "/wedding-card/static/media/Flaticon.15cf01b7.woff"
+  },
+  {
+    "revision": "7ad910ab912a8296b41d09126628d1a4",
+    "url": "/wedding-card/static/media/Flaticon.7ad910ab.ttf"
+  },
+  {
+    "revision": "b9b714e8ebdc31809b71111250ee257e",
+    "url": "/wedding-card/static/media/Flaticon.b9b714e8.eot"
+  },
+  {
+    "revision": "d069e66c4677076e61646ac80099683e",
+    "url": "/wedding-card/static/media/MyValentine_StandardQuality.d069e66c.mp3"
+  },
+  {
+    "revision": "cab771043aed5519705555d457194a12",
+    "url": "/wedding-card/static/media/Stelle.cab77104.mp3"
+  },
+  {
+    "revision": "ab6bac6eed179cc00c25c7dc0f34fcf0",
+    "url": "/wedding-card/static/media/T1.ab6bac6e.jpg"
+  },
+  {
+    "revision": "d07391307d42cd698eb93393817db648",
+    "url": "/wedding-card/static/media/T2.d0739130.jpg"
+  },
+  {
+    "revision": "89227d2c52b77fcb582d987f9f67de57",
+    "url": "/wedding-card/static/media/T3.89227d2c.jpg"
+  },
+  {
+    "revision": "c1d60c00f5f7ce639ad46752006eb5c5",
+    "url": "/wedding-card/static/media/T4.c1d60c00.jpg"
+  },
+  {
+    "revision": "90fcd9750cb941149f2d84f0112e4dae",
+    "url": "/wedding-card/static/media/T5.90fcd975.jpg"
+  },
+  {
+    "revision": "8e89450a2773ee0cf0c14cd0960e0df8",
+    "url": "/wedding-card/static/media/T6.8e89450a.jpg"
+  },
+  {
+    "revision": "14a85c461058ffdc25c9ba47b84f060d",
+    "url": "/wedding-card/static/media/T7.14a85c46.jpg"
+  },
+  {
+    "revision": "073a0b537f487781ce3b6f674769c46d",
+    "url": "/wedding-card/static/media/T8.073a0b53.jpg"
+  },
+  {
+    "revision": "427424af23b512374fdb91ea9a392716",
+    "url": "/wedding-card/static/media/T9.427424af.jpg"
+  },
+  {
+    "revision": "123b7d37cd8d85017b593ad45c328a13",
+    "url": "/wedding-card/static/media/close.123b7d37.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/wedding-card/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/wedding-card/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/wedding-card/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/wedding-card/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/wedding-card/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "8647dda6a6beb65dd4df53ff896342a9",
+    "url": "/wedding-card/static/media/google.8647dda6.png"
+  },
+  {
+    "revision": "74d7478371526214a09da9843d1af342",
+    "url": "/wedding-card/static/media/img-1.74d74783.jpg"
+  },
+  {
+    "revision": "6f24a898b26fc829e09960d89dec580d",
+    "url": "/wedding-card/static/media/img-2.6f24a898.jpg"
+  },
+  {
+    "revision": "c40e01c9b6c8bab947911c9b84db6a10",
+    "url": "/wedding-card/static/media/kakao.c40e01c9.png"
+  },
+  {
+    "revision": "cee3b7cc17ba25bbdacee45c9c1fd66c",
+    "url": "/wedding-card/static/media/naver.cee3b7cc.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/wedding-card/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/wedding-card/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/wedding-card/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/wedding-card/static/media/slick.f97e3bbf.svg"
+  }
+]);
